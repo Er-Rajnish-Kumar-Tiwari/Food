d5e9f452382abcb6d53ee5054ee0f0f58b7e8e2a
Rajnish Kumar Tiwari <tanish281202@gmail.com>
2025-03-31 19:57:43 +0100

    Update placeorder.jsx

--- fronted/src/Pages/Placeorder/placeorder.jsx
+++ fronted/src/Pages/Placeorder/placeorder.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-import './Placeorder.css'
+import './placeorder.css'
 import { StoreContext } from '../../Context/storeContext'
 import axios from 'axios';
 import { toast } from 'react-toastify';
